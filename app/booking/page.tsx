043fd2ddf5cfed9250c07659e45c0fbbdb85a2fe
Mohamed Mostafa jalal <mohamedmostafajalal@gmail.com>
2025-12-19 07:28:11 -0800

    Update page.tsx

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -1,6 +1,7 @@
+// app/booking/page.tsx
 import BookingClient from "./BookingClient";
 
-export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic"; // اجبار Next.js على CSR
 
 export default function BookingPage() {
   return <BookingClient />;
